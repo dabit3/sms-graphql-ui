66cc816058d912e57b4248eef1e50df9103683d6
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-09-05 08:52:25 -0700

    added bad word filter

--- lambda/index.js
+++ lambda/index.js
@@ -3,6 +3,11 @@ const gql = require('graphql-tag')
 const graphql = require('graphql')
 const { print } = graphql
 const uuid = require('uuid/v4')
+const Filter = require('bad-words')
+const filter = new Filter()
+
+var newBadWords = ['f', 'u', 'c', 'k', 's', 'h', 't', 'j', 'z', 'p', 'u', 's', 'y', 'fu', 'ck', 'uck', 'fuc', 'uc', 'shi', 'dic', 'ick', 'ck', 'ic', 'di', 'pus', 'uss', 'ussy', 'ssy', 'shi']
+filter.addWords(...newBadWords);
 
 const postMessage = gql`
   mutation createSMS($input: CreateSMSInput!) {
@@ -15,27 +20,25 @@ const postMessage = gql`
 
 exports.handler = async (event) => {
   const message = event['Records'][0]['Sns']['Message']
-  
   const data = JSON.parse(message)
-  
-  console.log('data:', data)
 
   const originationNumber = data["originationNumber"]
   const messageBody = data["messageBody"]
+  const cleanedMessage = filter.clean(messageBody)
 
   try {
     const graphqlData = await axios({
       url: '<API_URL>',
       method: 'post',
       headers: {
-        'x-api-key': '<API_KEY>'
+        'x-api-key': 'da2-a24rbpdvxjdcjcmaookatpez5i'
       },
       data: {
         query: print(postMessage),
         variables: {
           input: {
             id: uuid(),
-            messageBody,
+            messageBody: cleanedMessage,
             originationNumber
           }
         }
@@ -44,5 +47,6 @@ exports.handler = async (event) => {
     console.log('data successfully posted! :', graphqlData)
   } catch (err) {
     console.log('error posting to appsync: ', err)
-  }   
+  }
+    
 }
