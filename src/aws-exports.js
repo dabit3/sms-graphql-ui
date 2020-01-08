04e8ab8618d55329edcdf9cb4f445478e1ebc58f
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2020-01-07 17:23:46 -0800

    updated api key

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,7 @@ const awsmobile = {
   "aws_appsync_graphqlEndpoint": "https://qsup6kza5jgx3kl3nx7ybkrxse.appsync-api.us-east-1.amazonaws.com/graphql",
   "aws_appsync_region": "eu-central-1",
   "aws_appsync_authenticationType": "API_KEY",
-  "aws_appsync_apiKey": "da2-a24rbpdvxjdcjcmaookatpez5i"
+  "aws_appsync_apiKey": "da2-wqeyl6easzf4hc7uc2gmlazgau"
 };
 
 export default awsmobile;
